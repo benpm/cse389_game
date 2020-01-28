efa9f5457df49b2e27266a8d3bf7dc7965f6cd76
benpm <schwalegos@gmail.com>
2020-01-28 13:33:39 -0700

    added GameController object with singleton-like script thing

---
+++ game/Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController self;
+
+    //Enforce singleton behavior
+    void Awake()
+    {
+        if (!self)
+        {
+            self = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
