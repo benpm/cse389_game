484ba9af9c69aea1accc12df2b4e7ab671b62746
benpm <schwalegos@gmail.com>
2020-02-01 19:02:09 -0700

    train paths

---
+++ game/Assets/Scripts/TrainCar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainCar : MonoBehaviour
+{
+    private PathCreation.PathCreator path;
+
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        path = FindObjectOfType<PathCreation.PathCreator>();
+        Debug.Assert(path, "Train Path object not found");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = Time.timeSinceLevelLoad;
+        Vector3 point = path.path.GetPointAtDistance(t * speed);
+        transform.position = point;
+        Vector3 rotation = path.path.GetDirectionAtDistance(t * speed);
+        transform.rotation = Quaternion.LookRotation(rotation);
+    }
+}
